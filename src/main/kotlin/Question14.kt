95b0546e06b71c3816d572e8efa571d1c02ed6b6
prakash <prakashbohara18>
2020-12-12 10:00:28 +0545

    assignment1 completed

--- src/main/kotlin/Question14.kt
+++ src/main/kotlin/Question14.kt
@@ -2,11 +2,11 @@ fun main()
 {
     //Print all the even nos from an array
 
-    val array1=IntArray(size = 9)
+    val array1=IntArray( 9)
 
     print("keep entering number: ")
 
-    for (i in array1 . indices)
+    for (i in array1.indices)
     {
         array1[i]= readLine()!!.toInt()
     }
@@ -18,7 +18,7 @@ fun evenoarray(array:IntArray)
 {
     println("Even no in array are: ")
 
-    for(i in array. indices)
+    for(i in array.indices)
     {
         if(array[i]%2==0)
         {
